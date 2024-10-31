6d16f30361feece7d2218edc9ce6755655c58a56
Ian <60306464+iancanupp@users.noreply.github.com>
2024-10-30 18:51:55 -0700

    vault backup: 2024-10-30 18:51:55

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -29,12 +29,12 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#e0c9a6",
+          lightgray: "#474947",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
+          dark: "#1a1c1a",
+          secondary: "#68a2b9",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
